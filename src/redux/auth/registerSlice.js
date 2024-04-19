707d3b46b1a11369bfebf51a69817067c905fa0d
muneeb3123 <rehmanmuneeb66@gmail.com>
2024-04-19 14:28:16 +0500

    fix errors

--- src/redux/auth/registerSlice.js
+++ src/redux/auth/registerSlice.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 export const SignUpUser = createAsyncThunk('signup/fetch', async (formData) => {
   try {
-    const response = await axios.post('https://fast-lane-racers.onrender.com/signup', {
+    const response = await axios.post('http://localhost:3000/signup', {
       user: formData,
     });
     localStorage.setItem('token', response.headers.authorization);
